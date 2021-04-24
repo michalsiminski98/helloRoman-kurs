675dd5bbefbc8b1527bd4a6ca165ca0295763fad
Michal Siminski <michal.siminski3@gmail.com>
2021-04-24 08:32:59 +0200

    next lesson

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -1,8 +1,11 @@
 import { ReactComponent as DeleteIcon } from 'assets/icons/delete-icon.svg';
 import { StyledButton } from './Button.styles';
 
-const Button = () => (
-  <StyledButton>
+// click - onClick
+// mouseenter - onMouseEnter
+
+const Button = (props) => (
+  <StyledButton {...props}>
     <DeleteIcon />
   </StyledButton>
 );
